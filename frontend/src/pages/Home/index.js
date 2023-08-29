7cc1881c07d4770d1a07a66fcc5e122bcc0242c4
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-08-29 16:09:11 +0200

    course card and course card container wip

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,12 +1,13 @@
-import styles from "./home.module.scss";
+import CoursesCardTeaser from "./CoursesCardTeaser";
 import VideoBanner from "./VideoBanner";
+import styles from "./home.module.scss";
 const Home = () => {
   return (
     <div className={styles.homePageWrapper}>
       <div className={styles.homePage}>
-        <div>
-          <VideoBanner />
-        </div>
+        <VideoBanner />
+
+        <CoursesCardTeaser />
       </div>
     </div>
   );
