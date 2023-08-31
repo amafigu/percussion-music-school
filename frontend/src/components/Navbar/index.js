04ea15006b6b5a9fad4cd9d6d2bd67c70a0570f6
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-08-31 11:08:50 +0200

    dropdown for courses

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,22 +1,21 @@
-import { faGreaterThan } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { React, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import DropdownMenu from "./DropdownMenu";
 import styles from "./navbar.module.scss";
+
 const Navbar = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-
-  const toogleDropdown = () => {
-    setDropdownOpen((prevIsDropdownOpen) => !prevIsDropdownOpen);
-    console.log("toogleDropdown aft ", isDropdownOpen);
-  };
 
   const menuDropdownRef = useRef(null);
 
   return (
     <div className={styles.navbarWrapper}>
-      <nav className={styles.navbar}>
+      <nav
+        className={styles.navbar}
+        onMouseLeave={() => setDropdownOpen(false)}
+      >
         <div className={styles.column}>
           <img
             className={styles.logo}
@@ -29,12 +28,19 @@ const Navbar = () => {
             <span className={styles.linkContent}>about</span>
           </Link>
 
-          <div className={styles.link} onClick={() => toogleDropdown()}>
+          <div
+            className={styles.link}
+            onMouseEnter={() => setDropdownOpen(true)}
+          >
             <span className={styles.linkContent}>
-              programmes <FontAwesomeIcon icon={faGreaterThan} />
+              programmes <FontAwesomeIcon icon={faChevronDown} />
             </span>
             {isDropdownOpen && (
-              <div ref={menuDropdownRef} className={styles.menuDropdown}>
+              <div
+                ref={menuDropdownRef}
+                className={styles.menuDropdown}
+                onMouseLeave={() => setDropdownOpen(false)}
+              >
                 <DropdownMenu />
               </div>
             )}
