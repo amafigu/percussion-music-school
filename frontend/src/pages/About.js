019396f58c4c0bd6c57b3148dbfa9049028d5e08
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-09-05 12:39:08 +0200

    scholl section and styles refactoring

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -32,6 +32,71 @@ const About = () => {
             </div>
           </div>
         </div>
+        <div className={styles.linksRow}>
+          <ul>
+            <li className={styles.listItem}>
+              <a href='#school'>our school</a>
+            </li>
+            <li className={styles.listItem}>
+              <a href='#accolades'>our accolades</a>
+            </li>
+            <li className={styles.listItem}>
+              <a href='#philosophy'>our philosophy</a>
+            </li>
+            <li className={styles.listItem}>
+              <a href='#founder'>our founder</a>
+            </li>
+            <li className={styles.listItem}>
+              <a href='#story'>our story</a>
+            </li>
+          </ul>
+        </div>
+        <section className={styles.educationSection} id='school'>
+          <div className={styles.imageContainer}>
+            <img
+              alt='inspiration'
+              className={styles.imageEducation}
+              src={`${process.env.PUBLIC_URL}/assets/about_page_school.png`}
+            ></img>
+            <div className={styles.textLayer}>
+              <span className={styles.text}>
+                We seek to inspire and educate drummers by using their own
+                musicallity and from there to offer resources of contemporary
+                drum education.
+              </span>
+            </div>
+          </div>
+          <div className={styles.logoAndTextContainer}>
+            <div className={styles.logoAboveText}>
+              <div className={styles.logoContainer}>
+                <img
+                  className={styles.logo}
+                  alt='logo'
+                  src={`${process.env.PUBLIC_URL}/assets/logo.png`}
+                ></img>
+              </div>
+              <div className={styles.text}>Inspiration & Education</div>
+            </div>
+            <div className={styles.longText}>
+              <div className={styles.paragraphs}>
+                <p>
+                  At Tierra y Aire, we believe that the heartbeat of drumming
+                  begins with inspiration. Our programmes are created with
+                  creativity, innovation, originality, improvisation and
+                  extemporaneous drumming. We ensure the right balance for your
+                  specific needs at every drumming level.
+                </p>
+                <br />
+                <p>
+                  Co-developed by the students, our curriculum has been
+                  thoughtfully designed with your objectives in mind — we place
+                  a priority on your progress and create fun drumming
+                  experiences.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
