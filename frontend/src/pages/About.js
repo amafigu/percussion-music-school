30655c264b1ffd054273e10b162ae6daf2f82d06
Amadeus Figueiredo <amadeusfigue@gmail.com>
2023-09-10 23:08:26 +0200

    about page mobile done

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -114,7 +114,7 @@ const About = () => {
               </div>
               <div className={styles.text}>Inspiration & Education</div>
             </div>
-            <div className={styles.longText}>
+            <div className={styles.paragraphsContainer}>
               <div className={styles.paragraphs}>
                 <p>
                   At Tierra y Aire, we believe that the heartbeat of drumming
@@ -173,57 +173,66 @@ const About = () => {
             <div className={styles.imageContaier}>
               <img
                 alt='founder'
-                className={styles.image}
+                className={styles.imageForBigScreen}
                 src={`${process.env.PUBLIC_URL}/assets/card3.png`}
               />
+              <img
+                alt='founder'
+                className={styles.imageForMobileScreen}
+                src={`${process.env.PUBLIC_URL}/assets/about_page_founder_mobile.png`}
+              />
             </div>
-            <div className={styles.textAndIcons}>
-              <div className={styles.name}>Rolo Veron</div>
-              <div className={styles.position}>
-                Founder and Managing Director
-              </div>
-              <p>
-                The depth of Rolo’ musicality begins from his early musical
-                upbringing. Rolo discovered his love for music at a tender age
-                by experimenting with a many instruments such as electone organ,
-                guitar, bass, clarinet, flute, piano, drums, and percussion.
-              </p>
-              <p>
-                At 17, Rolo was introduced to master teacher Blair Sherrill, a
-                student of world-class drummer Super Drums and began private
-                drum lessons with Super Drums for four years in Rosario. He
-                started performing with Super Drums for bunch of stuff, and
-                gradually brought his performances to many campuses and around
-                town.
-              </p>
-              <div className={styles.socialIcons}>
-                <div className={styles.text}>Follow Rolo Veron @</div>
-                <div className={styles.row}>
-                  <div className={styles.icons}>
-                    <a
-                      href='https://www.instagram.com/rolo.veron/'
-                      target='_blanck'
-                    >
-                      <i
-                        className={`fab fa-instagram fa-2x ${styles.customIcon}`}
-                      ></i>
-                    </a>
-                    <a
-                      href='https://www.facebook.com/rolo.veron.3'
-                      target='_blanck'
-                    >
-                      <i
-                        className={`fab fa-facebook-f fa-2x ${styles.customIcon}`}
-                      ></i>
-                    </a>
-                    <a
-                      href='https://www.youtube.com/channel/UCAHUUHVEh7ov0qv8FIE11ZQ'
-                      target='_blanck'
-                    >
-                      <i
-                        className={`fab fa-youtube fa-2x ${styles.customIcon}`}
-                      ></i>
-                    </a>
+            <div className={styles.textAndIconsContainer}>
+              <div className={styles.textAndIcons}>
+                <div className={styles.name}>Rolo Veron</div>
+                <div className={styles.position}>
+                  Founder and Managing Director
+                </div>
+                <p>
+                  The depth of Rolo’ musicality begins from his early musical
+                  upbringing. Rolo discovered his love for music at a tender age
+                  by experimenting with a many instruments such as electone
+                  organ, guitar, bass, clarinet, flute, piano, drums, and
+                  percussion.
+                </p>
+                <br />
+                <p>
+                  At 17, Rolo was introduced to master teacher Blair Sherrill, a
+                  student of world-class drummer Super Drums and began private
+                  drum lessons with Super Drums for four years in Rosario. He
+                  started performing with Super Drums for bunch of stuff, and
+                  gradually brought his performances to many campuses and around
+                  town.
+                </p>
+                <div className={styles.socialIcons}>
+                  <div className={styles.text}>Follow Rolo Veron @</div>
+                  <div className={styles.row}>
+                    <div className={styles.icons}>
+                      <a
+                        href='https://www.instagram.com/rolo.veron/'
+                        target='_blanck'
+                      >
+                        <i
+                          className={`fab fa-instagram fa-2x ${styles.customIcon}`}
+                        ></i>
+                      </a>
+                      <a
+                        href='https://www.facebook.com/rolo.veron.3'
+                        target='_blanck'
+                      >
+                        <i
+                          className={`fab fa-facebook-f fa-2x ${styles.customIcon}`}
+                        ></i>
+                      </a>
+                      <a
+                        href='https://www.youtube.com/channel/UCAHUUHVEh7ov0qv8FIE11ZQ'
+                        target='_blanck'
+                      >
+                        <i
+                          className={`fab fa-youtube fa-2x ${styles.customIcon}`}
+                        ></i>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
