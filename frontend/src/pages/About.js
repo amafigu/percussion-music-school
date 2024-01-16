a5d2bcb6a7aa33f0d0e7aa27168d159f9af56be2
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-01-16 16:52:21 +0100

    dynamic text in about page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,16 +1,17 @@
+import OptionsToNavigate from "#components/OptionsToNavigate";
+import UpperBanner from "#components/UpperBanner";
+import useLocaleContext from "#context/localeContext";
 import {
   aboutPageNavigationItems,
   aboutPagePhilosophySectionBackground,
   aboutPageUpperSectionBackground,
-  aboutPageUpperSectionText,
-  aboutPageUpperSectionTitle,
 } from "#utils/constants";
 import { useEffectScrollTop } from "#utils/utils";
-import OptionsToNavigate from "../components/OptionsToNavigate";
-import UpperBanner from "../components/UpperBanner";
 import styles from "./about.module.scss";
 
 const About = () => {
+  const { translate } = useLocaleContext();
+  const text = translate.pages.about;
   useEffectScrollTop();
 
   const scrollToSection = (id) => {
@@ -25,8 +26,8 @@ const About = () => {
       <div className={styles.aboutPage}>
         <UpperBanner
           backgroundUrl={aboutPageUpperSectionBackground}
-          title={aboutPageUpperSectionTitle}
-          text={aboutPageUpperSectionText}
+          title={text.aboutPageUpperSectionTitle}
+          text={text.aboutPageUpperSectionText}
         />
         <OptionsToNavigate
           navigate={scrollToSection}
@@ -41,11 +42,7 @@ const About = () => {
             ></img>
 
             <div className={styles.textLayer}>
-              <span className={styles.text}>
-                We seek to inspire and educate drummers by using their own
-                musicallity and from there to offer resources of contemporary
-                drum education.
-              </span>
+              <span className={styles.text}>{text.firstImageCard}</span>
             </div>
           </div>
           <div className={styles.logoAndTextContainer}>
@@ -91,17 +88,11 @@ const About = () => {
               <span>Our Philosophy</span>
             </div>
             <div className={styles.subtitle}>
-              <span>Skill should not be harnessed by hard work alone.</span>
+              <span>{text.ourPhilosophySubtitle}</span>
             </div>
             <div className={styles.content}>
-              At Tierra y Aire, we believe that passion is the fundamental
-              building block to every student’s success. Through passion comes
-              curiosity, and it is this very curiosity that would spur the
-              student to continuously work, and learn, more on his or her craft.
-              It is our responsibility, as educators, to fuel that curiosity in
-              every student; constantly nurturing, guiding and motivating every
-              student to eventually reach their individual and collective music
-              objectives.
+              <p>{text.ourPhilosophyFirst}</p>
+              <p>{text.ourPhilosophySecond}</p>
             </div>
           </div>
         </section>
@@ -121,22 +112,9 @@ const About = () => {
                 <div className={styles.position}>
                   Founder and Managing Director
                 </div>
-                <p>
-                  The depth of Rolo’ musicality begins from his early musical
-                  upbringing. Rolo discovered his love for music at a tender age
-                  by experimenting with a many instruments such as electone
-                  organ, guitar, bass, clarinet, flute, piano, drums, and
-                  percussion.
-                </p>
+                <p>{text.artist.descriptionFirst}</p>
                 <br />
-                <p>
-                  At 17, Rolo was introduced to master teacher Blair Sherrill, a
-                  student of world-class drummer Super Drums and began private
-                  drum lessons with Super Drums for four years in Rosario. He
-                  started performing with Super Drums for bunch of stuff, and
-                  gradually brought his performances to many campuses and around
-                  town.
-                </p>
+                <p>{text.artist.descriptionSecond}</p>
                 <div className={styles.socialIcons}>
                   <div className={styles.text}>Follow Rolo Veron @</div>
                   <div className={styles.row}>
