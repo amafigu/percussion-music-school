e42364c9ee5e9244d148dc968f447c489ce82434
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-05-21 18:18:44 +0200

    add concerts mock

---
+++ frontend/src/__mocks__/concertsMock.js
@@ -0,0 +1,24 @@
+export const concertsMock = [
+  {
+    id: 2,
+    venue: "venue Berlin max",
+    venueLink: "www.venuelink.com",
+    city: "Berlin",
+    country: "Germany",
+    ticketsLink: "www.tickets.com",
+    concertDate: "2024-12-31T19:00:00.000+00:00",
+    createdAt: "2024-05-21T07:30:45.883+00:00",
+    updatedAt: "2024-05-21T07:30:45.883+00:00",
+  },
+  {
+    id: 3,
+    venue: "venue rosario max",
+    venueLink: "www.venuelinkrosario.com",
+    city: "Rosario",
+    country: "Argentine",
+    ticketsLink: "www.ticketsrosario.com",
+    concertDate: "2024-11-12T19:00:00.000+00:00",
+    createdAt: "2024-05-21T07:41:45.777+00:00",
+    updatedAt: "2024-05-21T07:41:45.777+00:00",
+  },
+];
