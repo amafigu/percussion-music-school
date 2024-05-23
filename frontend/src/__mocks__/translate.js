3c4d92563a4dcee7c21790839ea41b884c42e679
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-05-24 00:24:43 +0200

    course card tests

--- frontend/src/__mocks__/translate.js
+++ frontend/src/__mocks__/translate.js
@@ -3,14 +3,61 @@ export const translate = {
     courseCard: {
       seeMore: "See More",
     },
+    coursesCardTeaser: {
+      cards: {
+        ensemble: {
+          title: "Ensemble practice",
+          description: "share the music",
+        },
+        handTechnique: {
+          title: "Hand technique",
+          description: "The rhythm in your body",
+        },
+        stickTechnique: {
+          title: "Stick technique",
+          description: "control and dynamism",
+        },
+        musicalLanguage: {
+          title: "Musical language",
+          description: "understand music",
+        },
+        musicalDirection: {
+          title: "Musical direction",
+          description: "The expression in your hands",
+        },
+        ensembleComposition: {
+          title: "Ensemble composition",
+          description: "create your rhythms",
+        },
+      },
+    },
     concertsList: {
       ticketsLink: "tickets",
       venueLink: "venue",
+    },
+    navigationMenu: {
+      ensemble: "Ensemble practice",
+    },
+    testimonialCarousel: {
+      title: "testimonials",
     },
   },
   pages: {
     concerts: {
       title: "Concerts",
+    },
+    home: {
+      title: "Rosario's largest specialized drum school since 2018",
+      subtitle:
+        "Our mission is to inspire and educate drummers by using the latest technology and resources in providing a distinctive contemporary drum education.",
+    },
+    courses: {
+      musicalLanguage: {
+        title: "Musical language",
+        subtitle: "understand music",
+        description:
+          "In this course we will work on musical notation and its values, that is, we will learn to read and write music, emphasizing rhythm.",
+      },
     },
   },
 };
--- frontend/src/pages/Home/CourseCard/CourseCard.test.js
+++ frontend/src/pages/Home/CourseCard/CourseCard.test.js
@@ -1,22 +1,27 @@
 import { translate } from "#__mocks__/translate";
 import { useTranslate } from "#hooks/useTranslate";
-import { render, screen } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { MusicalLanguageCourses } from "#pages/MusicalLanguageCourses";
+import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  Link,
+  MemoryRouter,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import { CourseCard } from "./index";
 
 jest.mock("#hooks/useTranslate");
 
 describe("CourseCard", () => {
-  beforeEach(() => {
+  window.scrollTo = jest.fn();
+  test('has "see more" link', () => {
     useTranslate.mockReturnValue(translate);
-  });
-
-  test('renders "see more" link text', () => {
     const props = {
-      name: "testCourse",
-      description: "testDescription",
-      path: "/test-path",
-      image: "testImage.jpg",
+      name: "name",
+      description: "description",
+      path: "path",
+      image: "image",
     };
 
     render(
@@ -27,5 +32,49 @@ describe("CourseCard", () => {
 
     const linkElement = screen.getByText("See More");
     expect(linkElement).toBeInTheDocument();
+  });
+
+  test("has correct children and to property", () => {
+    render(
+      <Router>
+        <Link to='/courses/musical-language'>See More</Link>
+      </Router>,
+    );
+
+    expect(screen.getByRole("link")).toHaveTextContent("See More");
+    expect(screen.getByRole("link")).toHaveProperty(
+      "href",
+      "http://localhost/courses/musical-language",
+    );
+  });
+
+  test("navigates to the musical language page on link click", async () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <CourseCard
+                image='card1.png'
+                name='musicalLanguage'
+                path='/courses/musical-language'
+                description='Learn the language of music.'
+              />
+            }
+          />
+          <Route
+            path='/courses/musical-language'
+            element={<MusicalLanguageCourses />}
+          />
+        </Routes>
+      </MemoryRouter>,
+    );
+
+    const linkElement = screen.getByText("See More");
+    fireEvent.click(linkElement);
+
+    expect(screen.getByText("Musical Language")).toBeInTheDocument();
+    expect(screen.getByText("Understand Music")).toBeInTheDocument();
   });
 });
