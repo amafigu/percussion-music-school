b00b333ff45f0aac284393b5922240dad55d6793
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-05-24 14:06:07 +0200

    test MusicalLanguageCourses

--- frontend/src/pages/MusicalLanguageCourses/index.js
+++ frontend/src/pages/MusicalLanguageCourses/index.js
@@ -1,4 +1,4 @@
-import { musicalLanguageCoursesPageUpperSectionBackground } from "#constants/backgroundImages";
+import { musicalLanguageBackground } from "#constants/backgroundImages";
 import { handTechniqueCoursePageSecondImage } from "#constants/linkImages";
 import { CoursePageContent } from "#pages/CoursePageContent";
 import React from "react";
@@ -6,7 +6,7 @@ import React from "react";
 export const MusicalLanguageCourses = () => {
   return (
     <CoursePageContent
-      upperBannerImgPath={musicalLanguageCoursesPageUpperSectionBackground}
+      upperBannerImgPath={musicalLanguageBackground}
       courseBannerImgPath={handTechniqueCoursePageSecondImage}
       pageTranslationObj={"musicalLanguage"}
       invertLayout={true}
