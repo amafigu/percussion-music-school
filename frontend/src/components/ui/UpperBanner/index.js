ddd20ba7a2e7f582f5fae5b7f2e4931cba119a05
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-05-24 18:59:13 +0200

    pass only string as background img props

--- frontend/src/components/ui/UpperBanner/index.js
+++ frontend/src/components/ui/UpperBanner/index.js
@@ -7,7 +7,13 @@ export const UpperBanner = ({ backgroundUrl, title, text }) => {
     <section className={styles.coursesPageWrapper} aria-label={title}>
       <div className={styles.coursesPage}>
         <div className={styles.upperSection}>
-          <div className={styles.backgroundImage} style={backgroundUrl}></div>
+          <div
+            className={styles.backgroundImage}
+            aria-label='background'
+            style={{
+              backgroundImage: `url(${process.env.PUBLIC_URL}${backgroundUrl})`,
+            }}
+          ></div>
           <div className={styles.text}>
             <div className={styles.title}>{camelCaseToTitleCase(title)}</div>
             <div className={styles.content}>{camelCaseToTitleCase(text)}</div>
